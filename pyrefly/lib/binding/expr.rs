364ada7e89b8640c0c56a834d981ae40bf883b1f
Asuka Minato <i@asukaminato.eu.org>
2026-05-20 16:09:20 +0900

    .

--- pyrefly/lib/binding/expr.rs
+++ pyrefly/lib/binding/expr.rs
@@ -1336,7 +1336,7 @@ impl<'a> BindingsBuilder<'a> {
     }
 
     fn attrs_default_decorator_field(&self, expr: &Expr) -> Option<Name> {
-        if self.scopes.enclosing_class_and_metadata_keys().is_none() {
+        if self.scopes.enclosing_class_object_idx().is_none() {
             return None;
         }
         let Expr::Attribute(attr) = expr else {
